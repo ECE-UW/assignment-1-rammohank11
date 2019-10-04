dfdc35c50b683fc9358adcded436f99c4f8a8f0b
rammohank11 <55061635+rammohank11@users.noreply.github.com>
2019-10-03 23:55:37 -0400

    regrade

--- a1ece650.py
+++ a1ece650.py
@@ -1,8 +1,7 @@
 # The output is an Undirected Graph
 import sys
-import numpy
+import numpy as np
 import re
-
 
 def adding_lines(a, dblines):
     cmd_name = '"(.*)"'
@@ -10,8 +9,12 @@ def adding_lines(a, dblines):
     street_name = re.findall(cmd_name, a)
     street_node = re.findall(cmd_node, a)
     street_name = "".join(street_name)
-    dblines[street_name] = street_node
-
+    if street_name not in dblines:
+        dblines[street_name] = street_node
+    else:
+        print 'Error: street currently exists.'
+    if a[-1] != ')':
+        print 'Error: Incorrect input format'
 
 def change_lines(c, dblines):
     cmd_name = '"(.*)"'
@@ -19,61 +22,59 @@ def change_lines(c, dblines):
     street_name = re.findall(cmd_name, c)
     street_node = re.findall(cmd_node, c)
     street_name = "".join(street_name)
-
-    for things in dblines:
-        if street_name == things:
-            dblines[things] = street_node
-
-
-def remove_lines(r, dblines):
-    try:
-        cmd_name = re.compile('"(.*)"')
-        street_name = "".join(cmd_name.findall(r))
-        street_name = "".join(street_name)
-
+    if street_name not in dblines:
+        print "Error: 'c' or 'r' specified for a street that does not exist"
+    else:
         for things in dblines:
             if street_name == things:
-                del dblines[street_name]
-            break
-    except ValueError:
-        print "invalid command"
+                   dblines[things] = street_node
 
+def remove_lines(r, dblines):
+    cmd_name = re.compile('"(.*)"')
+    street_name = "".join(cmd_name.findall(r))
+    street_name = "".join(street_name)
+    if street_name not in dblines:
+        print "Error: 'c' or 'r' specified for a street that does not exist"
+    else:
+        for things in dblines:
+         if street_name == things:
+            del dblines[street_name]
+         break
 
 def main():
-    Db_street_node = {}
-    while True:
-        line = raw_input('')
-        if line == '':
-            break
-        elif line[0] == 'a' and line[1] == ' ':
-            adding_lines(line, Db_street_node)
-        elif line[0] == 'c' and line[1] == ' ':
-            change_lines(line, Db_street_node)
-        elif line[0] == 'r' and line[1] == ' ':
-            remove_lines(line, Db_street_node)
-        elif line[0] == 'g':
-            if line[1:] == '':
-                vertex = Db_street_node.values()
-                vindex = len(Db_street_node)
-                print "V:{"
-                print vertex
-                print "}"
-                print "E:{"
-                #for items in Db_street_node:
-                print "<","1","," "2" ,">"
-                print "<","2","," "4" ,">"
-                print "<", "2", "," "3", ">"
-                print "}"
-            else:
-                print('Error: command not exist.')
-        elif line[0] != 'r' or 'a' or 'c' or 'r' or 'g' and line[1] == ' ':
-            print "Error: Invalid command requested"
+        Db_street_node = {}
+        try :
+            while True:
+                line = raw_input('')
+                if line == '':
+                    break
+                elif line[0] == 'a' and line[1:] != ' ':
+                    adding_lines(line, Db_street_node)
+                elif line[0] == 'c' and line[1] == ' ':
+                    change_lines(line, Db_street_node)
+                elif line[0] == 'r' and line[1] == ' ':
+                    remove_lines(line, Db_street_node)
+                elif line[0] == 'g':
+                    if line[1:] == '':
+                        vertex = Db_street_node.values()
+                        print "V={"
+                        #print vertex
+                        print "}"
+                        print "E={"
+                        #for items in Db_street_node:
+                        ##
+                    else:
+                        print('Error: Incorrect input Format')
+                elif line[0] != 'r' or 'a' or 'c' or 'r' or 'g' and line[1] == ' ':
+                    print "Error: Incorrect input format"
 
-        elif line == '':
-            break
-        else:
-            pass
-    sys.exit(0)
+                elif line == '':
+                    break
+                else:
+                    pass
+            sys.exit(0)
+        except:
+            print '\n'
 
 
 if __name__ == '__main__':
